00ff3d0c2da4384724869f59a5241f908922d1ad
Prakash Acharya <pracharya2601@gmail.com>
2020-03-29 16:16:28 -0700

    package.json

--- server.js
+++ server.js
@@ -24,4 +24,6 @@ db.once('open', () => console.log("Connected to mongoose"))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
